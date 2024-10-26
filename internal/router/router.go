b737236489713d7344869da3b6ff54fecab2f7d5
Андрей Смирнов <andrey.smirnov@realize.dev>
2024-10-26 19:51:31 +0300

    fix update

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/SmirnovND/metrics/internal/repo"
 	"github.com/SmirnovND/metrics/internal/services/server"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
@@ -13,11 +14,12 @@ func Handler(storage *repo.MemStorage) http.Handler {
 	metricController := controllers.NewMetricsController(serviceCollector)
 
 	r := chi.NewRouter()
+	r.Use(middleware.StripSlashes)
 	r.Post("/update/{type}/{name}/{value}", metricController.HandleUpdate)
-	r.Post("/update/", metricController.HandleUpdateJSON)
+	r.Post("/update", metricController.HandleUpdateJSON)
 	r.Get("/value/{type}/{name}", metricController.HandleValue)
 	r.Post("/value/{type}/{name}", metricController.HandleValueJSON)
-	r.Post("/value/", metricController.HandleValueJSON)
+	r.Post("/value", metricController.HandleValueJSON)
 
 	// Обработчик для неподходящего метода (405 Method Not Allowed)
 	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
