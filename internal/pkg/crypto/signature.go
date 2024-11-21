36282d2e090add26ebf876c979cc51b28fc5dfd5
Андрей Смирнов <andrey.smirnov@realize.dev>
2024-11-21 12:37:24 +0300

    iter14 crypto

--- internal/pkg/crypto/signature.go
+++ internal/pkg/crypto/signature.go
@@ -69,8 +69,9 @@ func WithCryptoKey(config interfaces.ConfigServer, next http.Handler) http.Handl
 
 		hash := r.Header.Get("HashSHA256")
 		if hash == "" {
-			fmt.Println("__________________________Missing HashSHA256 header____________________________")
-			http.Error(w, "Missing HashSHA256 header", http.StatusBadRequest)
+			//fmt.Println("__________________________Missing HashSHA256 header____________________________")
+			//http.Error(w, "Missing HashSHA256 header", http.StatusBadRequest)
+			next.ServeHTTP(w, r)
 			return
 		}
 
