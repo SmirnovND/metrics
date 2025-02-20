6c969db245a1b4d5dbd0da144cf0a13e385dbc79
Андрей Смирнов <andrey.smirnov@realize.dev>
2025-02-20 15:49:36 +0300

    серты не запустились

--- internal/services/agent/sender.go
+++ internal/services/agent/sender.go
@@ -18,6 +18,30 @@ import (
 	"os"
 )
 
+// Send Метод для отправки метрик
+func Send(m *domain.Metrics, serverHost string) {
+	m.Mu.RLock()         // Блокируем доступ к мапе
+	defer m.Mu.RUnlock() // Освобождаем доступ после обновления
+
+	for _, metric := range m.Data {
+		url := fmt.Sprintf("%s/update/%s/%s/%v", serverHost, metric.GetType(), metric.GetName(), metric.GetValue())
+
+		// Создание HTTP-запроса
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte{}))
+		if err != nil {
+			fmt.Println("Error creating request:", err)
+			continue
+		}
+
+		req.Header.Set("Content-Type", "text/plain")
+
+		err = baseSend(req, true)
+		if err != nil {
+			continue
+		}
+	}
+}
+
 // Генерация случайного симметричного ключа для AES
 func generateAESKey() ([]byte, error) {
 	key := make([]byte, 32) // 256 бит для AES
