465e722648a9ba2cfc70562663838ba5781c9d5e
Андрей Смирнов <andrey.smirnov@realize.dev>
2025-02-20 15:36:36 +0300

    config

--- internal/pkg/config/server/config.go
+++ internal/pkg/config/server/config.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/SmirnovND/metrics/internal/interfaces"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"time"
@@ -11,13 +14,13 @@ import (
 const DefaultStoreInterval = 300
 
 type Config struct {
-	StoreInterval   int
-	FileStoragePath string
-	Restore         bool
-	FlagRunAddr     string
-	DBDsn           string
-	Key             string
-	CryptoKey       string
+	StoreInterval   int    `json:"store_interval"`
+	FileStoragePath string `json:"store_file"`
+	Restore         bool   `json:"restore"`
+	FlagRunAddr     string `json:"address"`
+	DBDsn           string `json:"database_dsn"`
+	Key             string `json:"key"`
+	CryptoKey       string `json:"crypto_key"`
 }
 
 func (c *Config) GetStoreInterval() time.Duration {
@@ -39,9 +42,11 @@ func (c *Config) GetFlagRunAddr() string {
 func (c *Config) GetDBDsn() string {
 	return c.DBDsn
 }
+
 func (c *Config) GetKey() string {
 	return c.Key
 }
+
 func (c *Config) GetCryptoKey() string {
 	return c.CryptoKey
 }
@@ -56,9 +61,11 @@ func NewConfigCommand() (cf interfaces.ConfigServerInterface) {
 	flag.StringVar(&config.FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&config.Key, "k", "", "key")
 	flag.StringVar(&config.CryptoKey, "crypto-key", "", "crypto-key")
+	configFile := flag.String("c", "", "path to config file")
 
 	flag.Parse()
 
+	// Read config from environment variables
 	if StoreInterval := os.Getenv("STORE_INTERVAL"); StoreInterval != "" {
 		StoreIntervalInt, err := strconv.Atoi(StoreInterval)
 		if err == nil {
@@ -75,9 +82,9 @@ func NewConfigCommand() (cf interfaces.ConfigServerInterface) {
 	}
 
 	if Restore := os.Getenv("RESTORE"); Restore != "" {
-		Restore, err := strconv.ParseBool(Restore)
+		RestoreBool, err := strconv.ParseBool(Restore)
 		if err == nil {
-			config.Restore = Restore
+			config.Restore = RestoreBool
 		}
 	}
 
@@ -91,6 +98,50 @@ func NewConfigCommand() (cf interfaces.ConfigServerInterface) {
 
 	if envCryptoKey := os.Getenv("CRYPTO_KEY"); envCryptoKey != "" {
 		config.CryptoKey = envCryptoKey
+	}
+
+	// Если файл конфигурации указан, загружаем его
+	if *configFile != "" {
+		file, err := os.Open(*configFile)
+		if err != nil {
+			fmt.Println("Ошибка открытия файла конфигурации:", err)
+			return nil
+		}
+		defer file.Close()
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println("Ошибка чтения файла конфигурации:", err)
+			return nil
+		}
+
+		// Парсим JSON из файла и применяем
+		var fileConfig Config
+		err = json.Unmarshal(data, &fileConfig)
+		if err != nil {
+			fmt.Println("Ошибка парсинга JSON из конфигурационного файла:", err)
+			return nil
+		}
+
+		// Check if the values are empty and only update empty fields
+		if config.StoreInterval == 0 {
+			config.StoreInterval = fileConfig.StoreInterval
+		}
+		if config.FileStoragePath == "" {
+			config.FileStoragePath = fileConfig.FileStoragePath
+		}
+		if config.FlagRunAddr == "" {
+			config.FlagRunAddr = fileConfig.FlagRunAddr
+		}
+		if config.DBDsn == "" {
+			config.DBDsn = fileConfig.DBDsn
+		}
+		if config.Key == "" {
+			config.Key = fileConfig.Key
+		}
+		if config.CryptoKey == "" {
+			config.CryptoKey = fileConfig.CryptoKey
+		}
 	}
 
 	return config
